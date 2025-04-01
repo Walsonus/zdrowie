bd83a95f69d2595322b70a87d54f7b0274ff18d7
Krzysztof <k.sokalski@onet.pl>
2025-04-01 15:29:52 +0200

    -create login activity -create listeners for the buttons -add app logo -create login page layout and style -implement polish and english localisation in strings.xml -add basic colors in colors.xml -add basic styles in styles.xml

---
+++ app/src/main/java/pack/zdrowie/LoginActivity.kt
@@ -0,0 +1,55 @@
+package pack.zdrowie
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputLayout
+import pack.zdrowie.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        // View binding initialization
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Edge-to-edge handling
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        setupViews()
+    }
+
+    private fun setupViews() {
+        // Login button click listener
+        binding.loginButton.setOnClickListener {
+            if (validateForm()) {
+                performLogin()
+            }
+        }
+    }
+
+    // validateForm checks if user gave correct login data
+    private fun validateForm(): Boolean {
+        return true
+    }
+
+    // performLogin makes user logged in and changes activity
+    private fun performLogin() {
+
+    }
+
+
+}
