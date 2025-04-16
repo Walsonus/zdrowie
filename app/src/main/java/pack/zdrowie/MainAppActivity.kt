f27057c0329bdc54f0390e02921aec1c729c0fe9
Krzysztof <k.sokalski@onet.pl>
2025-04-16 19:12:19 +0200

    -add icons in main activity -edit strings.xml and english translations -add material library in build.gradle.kts -remove grid view from main activity -edit colors in main activity -adjust layout in main activity -add wider logo png file to the project

--- app/src/main/java/pack/zdrowie/MainAppActivity.kt
+++ app/src/main/java/pack/zdrowie/MainAppActivity.kt
@@ -6,6 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import pack.zdrowie.databinding.ActivityMainAppBinding
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.GridView
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,4 +26,7 @@ class MainAppActivity : AppCompatActivity() {
             insets
         }
     }
+
+
+
 }
