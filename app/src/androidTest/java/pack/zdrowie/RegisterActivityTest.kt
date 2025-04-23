663e3f59166528546a111f2fd25ec60eff05910e
Walsonus <walkiewiczbartosz@gmail.com>
2025-04-23 14:27:46 +0200

    Update: LoginActivityTests, RegisterActivityTests

--- app/src/androidTest/java/pack/zdrowie/RegisterActivityTest.kt
+++ app/src/androidTest/java/pack/zdrowie/RegisterActivityTest.kt
@@ -1,65 +1,133 @@
 package pack.zdrowie
-
+import android.app.Activity
+import android.view.View
+import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.RootMatchers
+import androidx.test.espresso.matcher.RootMatchers.withDecorView
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import junit.framework.TestCase.assertNotNull
-import org.junit.After
-import org.junit.Before
+import org.hamcrest.Matchers
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.not
 import org.junit.Rule
 import org.junit.Test
+import androidx.test.espresso.intent.matcher.IntentMatchers.*
+
 
 class RegisterActivityTest {
-    @get:Rule
+
+    @Rule
+    @JvmField
     val activityRule = ActivityScenarioRule(RegisterActivity::class.java)
 
-    @Before
-    fun setUp() {
-        Intents.init()
-    }
-
-    @After
-    fun tearDown() {
-        Intents.release()
-    }
-
     @Test
-    fun checkLoginButtonIsDisplayed() {
-        onView(withId(R.id.loginButton)).check(matches(isDisplayed()))
-    }
+    fun register_withEmptyEmail_showsError() {
+        onView(withId(R.id.emailTextEdit)).perform(typeText(""), closeSoftKeyboard())
+        onView(withId(R.id.passwordTextEdit2)).perform(typeText("password123"), closeSoftKeyboard())
+        onView(withId(R.id.passwordTextEdit3)).perform(typeText("password123"), closeSoftKeyboard())
+        onView(withId(R.id.loginButton)).perform(click())
 
-    @Test
-    fun checkEmailLayoutIsDisplayed() {
-        onView(withId(R.id.emailLayout)).check(matches(isDisplayed()))
-    }
-
-    @Test
-    fun checkPasswordLayoutIsDisplayed() {
-        onView(withId(R.id.passwordLayout)).check(matches(isDisplayed()))
-    }
-
-    @Test
-    fun checkEmailEditTextIsDisplayed() {
-        onView(withId(R.id.emailTextEdit)).check(matches(isDisplayed()))
-    }
-
-    @Test
-    fun checkRepeatPasswordLayoutIsDisplayed() {
-        onView(withId(R.id.passwordTextEdit3)).check(matches(isDisplayed()))
-    }
-
-    @Test
-    fun bindingIsInitializedInOnCreate() {
         activityRule.scenario.onActivity { activity ->
-            assertNotNull(activity.getBinding())
+            try {
+                onView(withText(R.string.no_email))
+                    .inRoot(RootMatchers.withDecorView(Matchers.not(activity.window.decorView)))
+                    .check(matches(isDisplayed()))
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+
+    }
+
+    @Test
+    fun register_withInvalidEmail_showsError() {
+        onView(withId(R.id.emailTextEdit)).perform(typeText("invalid-email"), closeSoftKeyboard())
+        onView(withId(R.id.passwordTextEdit2)).perform(typeText("password123"), closeSoftKeyboard())
+        onView(withId(R.id.passwordTextEdit3)).perform(typeText("password123"), closeSoftKeyboard())
+        onView(withId(R.id.loginButton)).perform(click())
+
+        activityRule.scenario.onActivity { activity ->
+            try {
+                onView(withText(R.string.wrong_email))
+                    .inRoot(RootMatchers.withDecorView(Matchers.not(activity.window.decorView)))
+                    .check(matches(isDisplayed()))
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+    }
+
+    @Test
+    fun register_withNotEqualPasswords_showsError() {
+        onView(withId(R.id.emailTextEdit)).perform(
+            typeText("test@example.com"),
+            closeSoftKeyboard()
+        )
+        onView(withId(R.id.passwordTextEdit2)).perform(typeText("password123"), closeSoftKeyboard())
+        onView(withId(R.id.passwordTextEdit3)).perform(typeText("password124"), closeSoftKeyboard())
+        onView(withId(R.id.loginButton)).perform(click())
+
+        activityRule.scenario.onActivity { activity ->
+            try {
+                onView(withText(R.string.mismatch_password))
+                    .inRoot(RootMatchers.withDecorView(Matchers.not(activity.window.decorView)))
+                    .check(matches(isDisplayed()))
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
     }
 
+    @Test
+    fun register_withNoPasswords_showsError() {
+        onView(withId(R.id.emailTextEdit)).perform(
+            typeText("test@example.com"),
+            closeSoftKeyboard()
+        )
+        onView(withId(R.id.passwordTextEdit2)).perform(typeText(""), closeSoftKeyboard())
+        onView(withId(R.id.passwordTextEdit3)).perform(typeText(""), closeSoftKeyboard())
+        onView(withId(R.id.loginButton)).perform(click())
 
+        activityRule.scenario.onActivity { activity ->
+            try {
+                onView(withText(R.string.no_password))
+                    .inRoot(RootMatchers.withDecorView(Matchers.not(activity.window.decorView)))
+                    .check(matches(isDisplayed()))
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    @Test
+    fun register_withCorrectCredentials() {
+        onView(withId(R.id.emailTextEdit)).perform(
+            typeText("test@example.com"),
+            closeSoftKeyboard()
+        )
+        onView(withId(R.id.passwordTextEdit2)).perform(typeText(""), closeSoftKeyboard())
+        onView(withId(R.id.passwordTextEdit3)).perform(typeText(""), closeSoftKeyboard())
+        onView(withId(R.id.loginButton)).perform(click())
+
+        activityRule.scenario.onActivity { activity ->
+            try {
+                onView(withText(R.string.user_registered))
+                    .inRoot(RootMatchers.withDecorView(Matchers.not(activity.window.decorView)))
+                    .check(matches(isDisplayed()))
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
 }
+
--- app/src/androidTest/java/pack/zdrowie/ToastMatcher.kt
+++ app/src/androidTest/java/pack/zdrowie/ToastMatcher.kt
@@ -1,28 +1,25 @@
+package pack.zdrowie
 
-//Class for 'TEST FOR DISPLAY INCORRECT DATA'
-//package pack.zdrowie
-//
-//import android.view.WindowManager
-//import androidx.test.espresso.Root
-//import org.hamcrest.Description
-//import org.hamcrest.TypeSafeMatcher
-//
-//class ToastMatcher : TypeSafeMatcher<Root>() {
-//    override fun describeTo(description: Description) {
-//        description.appendText("is toast")
-//    }
-//
-//    public override fun matchesSafely(root: Root): Boolean {
-//        val type = root.windowLayoutParams?.get()?.type
-//        if (type == WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY ||
-//            type == WindowManager.LayoutParams.TYPE_APPLICATION_ATTACHED_DIALOG
-//        ) {
-//            return true
-//        }
-//
-//        val windowToken = root.decorView.windowToken
-//        val appToken = root.decorView.applicationWindowToken
-//        return windowToken !== appToken
-//    }
-//}
+import android.os.Build
+import android.view.WindowManager
+import androidx.test.espresso.Root
+import org.hamcrest.Description
+import org.hamcrest.TypeSafeMatcher
 
+class ToastMatcher : TypeSafeMatcher<Root>() {
+
+    override fun describeTo(description: Description) {
+        description.appendText("is toast")
+    }
+
+    override fun matchesSafely(root: Root): Boolean {
+        val windowLayoutParamsType = root.windowLayoutParams.get()?.type
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            // Użycie TYPE_APPLICATION_OVERLAY dla API 30+
+            windowLayoutParamsType == WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+        } else {
+            // Użycie TYPE_TOAST dla starszych API
+            windowLayoutParamsType == WindowManager.LayoutParams.TYPE_TOAST
+        }
+    }
+}
