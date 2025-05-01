e77ec2a496e01872c8982b2518a93a4340639acb
Hubert <hubert.kosiorowski1@gmail.com>
2025-05-02 01:17:41 +0200

    added: -data classes for meal and activity, -basics of logic for kcal tracker.

---
+++ app/src/main/java/pack/zdrowie/caloriesTracker/caloriesTracker.kt
@@ -0,0 +1,27 @@
+package pack.zdrowie.caloriesTracker
+
+class caloriesTracker {
+
+    private val meals = mutableListOf<Meal>()
+    private val activities = mutableListOf<Activity>()
+
+    fun addMeal(meal: Meal) {
+        meals.add(meal)
+    }
+
+    fun addActivity(activity: Activity) {
+        activities.add(activity)
+    }
+
+    fun totalCaloriesEaten(): Float {
+        return meals.sumOf { it.totalKcal.toDouble() }.toFloat()
+    }
+
+    fun totalCaloriesBurned(): Float {
+        return activities.sumOf { it.kcalBurned.toDouble() }.toFloat()
+    }
+
+    fun netCalories(): Float {
+        return totalCaloriesEaten() - totalCaloriesBurned()
+    }
+}
